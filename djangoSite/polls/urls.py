ef7f2e2a4e3429125cdff86c9ecf0877e8812812
yoav cohen <yoac@Post.bgu.ac.il>
2017-07-06 01:09:56 +0200

    add ui and server support to watch songs lyrics

--- djangoSite/polls/urls.py
+++ djangoSite/polls/urls.py
@@ -1,16 +1,14 @@
 from django.conf.urls import url
 from django.contrib import admin
-
-
 from . import views
 from . import helper
 
 urlpatterns = [
     # ex: /polls/
-    url(r'^$', views.index, name='index'),
+    url('index', views.index, name='index'),
     # ex: /polls/5/
     url(r'^(?P<file_name_to_open>[a-z]+)/updateDb/$', helper.insert_to_db_from_file, name='insert to db from file'),
     # ex: /polls/5/results/
-    url(r'^(?P<song_id>[0-9]+)/results/$', views.results, name='results'),
+    url(r'^findbyid/([0-9]+)/$', views.find_song, name = 'find by id'),
     url(r'^admin/', admin.site.urls),
 ]
--- djangoSite/polls/views.py
+++ djangoSite/polls/views.py
@@ -6,19 +6,29 @@ from django.shortcuts import render
 def cityName(request, song_cities):
     return HttpResponse("You're looking at song")
 
-
 def results(request, song_id):
     response = "You're looking at the cities of song %s."
     return HttpResponse(response % song_id)
 
+def song_list(request):
+	return HttpResponse(Song.objects.all())
 
 def index(request):
     all_songs = Song.objects.all()
-    output = ', '.join([q.song_artist for q in all_songs])
+    songs_name = [q.song_name for q in all_songs]
+    songs_ids = [q.id for q in all_songs]
     # Render the HTML template index.html with the data in the context variable
     return render(
         request,
         'index.html',
-        context={'all_songs':all_songs},
+        context = {'songs_name':Song.objects.all() ,'songs_id':songs_ids },
     )
 
+def find_song(request, song_id):
+    # Render the HTML template index.html with the data in the context variable
+    _song = Song.get_song_by_id(song_id)
+    return render(
+        request,
+        'song.html',
+        context = {'song' : _song },
+    )
