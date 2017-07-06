7ba7261b16a1ce7561a51bd00dbebd5989d276d5
Aviv Gorlik <avivgorlik21@gmail.com>
2017-07-06 14:32:01 +0300

    search text box

--- djangoSite/polls/urls.py
+++ djangoSite/polls/urls.py
@@ -6,7 +6,7 @@ from . import helper
 urlpatterns = [
     # ex: /polls/
     url('index', views.index, name='index'),
-    # ex: /polls/5/
+    url('search', views.search, name='search'),
     url(r'^addSongsToDbFromDbFolder/', helper.insert_songs_to_db_from_db_folder, name='insert to db all songs from file'),
     # ex: /polls/5/results/
     url(r'^findbyid/([0-9]+)/$', views.find_song_by_id, name = 'find by id'),
--- djangoSite/polls/views.py
+++ djangoSite/polls/views.py
@@ -26,12 +26,38 @@ def index(request):
     )
 
 
+def search(request):
+    print("DEBUG: SEARCH VIEWER")
+    songs_to_show = Song.objects.all()
+    artist = request.GET.get('search_box_artist')
+    song_name = request.GET.get('search_box_song')
+    if artist is not None:
+        print("ASA")
+        songs_to_show = Song.objects.get(song_artist=artist)
+        print("kkk")
+        print(songs_to_show)
+
+    if song_name is not None:
+        print("ASA")
+        songs_to_show = Song.objects.get(song_artist=artist)
+        print("kkk")
+        print(songs_to_show)
+
+    songs_ids = [q.id for q in songs_to_show]
+    # Render the HTML template index.html with the data in the context variable
+    return render(
+        request,
+        'search.html',
+        context={'songs_list': songs_to_show,'songs_id': songs_ids},
+    )
+
+
 def find_song_by_id(request, song_id):
     # Render the HTML template index.html with the data in the context variable
     _song = Song.get_song_by_id(song_id)
     return render(
         request,
-        'song.html',
+        'search.html',
         context={'song': _song},
     )
 
@@ -41,7 +67,7 @@ def find_song_by_name(request, song_name):
     _song = Song.get_song_by_name(song_name)
     return render(
         request,
-        'song.html',
+        'search.html',
         context={'song': _song},
     )
 
@@ -51,6 +77,6 @@ def find_song_by_artist(request, song_artist):
     _song = Song.get_song_by_artist(song_artist)
     return render(
         request,
-        'searc.html',
+        'search.html',
         context={'song': _song},
     )
